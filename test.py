77866e57f3a19aee1262085935f7eb81eff621a2
Mahnatse <mahnatse.choung@umuzi.org>
2019-11-18 14:38:01 +0200

    first Commit

---
+++ test.py
@@ -0,0 +1,19 @@
+from test_function import *
+
+def test_addition():
+
+    assert addition(0,0) == 0
+    assert addition(-1,-1) == -2
+    assert addition(4,5) == 9
+    assert addition(1,2,3,4) == 10
+
+def test_multiplying():
+
+    assert multiplying(1,2) == 2
+    assert multiplying(-1,2) == -2
+    assert multiplying(4,5) == 20
+    assert multiplying(1,2,3,4) == 24
+
+
+
+   
---
+++ test_function.py
@@ -0,0 +1,29 @@
+#Addition function
+
+def addition(*a):
+
+    addition = 0
+
+    for num in a:
+
+        addition += num
+
+    return addition 
+
+'''print(addition(4,5))''' 
+
+#Multiplication Function
+
+def multiplying (*m):
+
+    multiplying = 1
+
+    for num in m:
+
+        multiplying *= num
+    
+    return multiplying
+
+print(multiplying(4.3,5))
+
+
